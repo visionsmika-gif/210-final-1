8e25e2d37c73db86b45227d62c6dbb5988216b3c
visionsmika-gif <visionsmika@gmail.com>
2025-12-08 17:20:20 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -10,11 +10,12 @@ using namespace std;
 
 
 int main() {
+	// Milestone 1
 	const string FILE_NAME = "data.txt";
 	map<string, int> traffic;
 
 	ifstream inputFile(FILE_NAME);
-	if (!inputsFile) {
+	if (!inputFile) {
 		cout << "ERROR: Unable to open the file " << FILE_NAME << ".\n";
 		return 1;
 	}
@@ -24,12 +25,20 @@ int main() {
 	while (inputFile >> origin) {
 		inputFile >> dest;
 		
-		traffic[origin] = 0;
-		traffic[dest] = 0;
-
-
+		if (traffic.find(origin) == traffic.end()) {
+			traffic[origin] = 0;
+		}
+		if (traffic.find(dest) == traffic.end()) {
+			traffic[dest] = 0;
+		}
+		++traffic[origin];
+		++traffic[dest];
 	}
 
+	cout << "All airport traffic counts:\n";
+	for (const auto& pair : traffic) {
+		cout << pair.first << " " << pair.second << "\n";
+	}
 
 
 	return 0;

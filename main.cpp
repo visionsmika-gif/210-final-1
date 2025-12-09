5935dd9fe6f8bfd95d214f620b8a065ed46f4b71
visionsmika-gif <visionsmika@gmail.com>
2025-12-08 17:14:32 -0800

    update

--- main.cpp
+++ main.cpp
@@ -2,10 +2,35 @@
 // IDE used: Visual Studio 2022
 
 #include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
+
 int main() {
+	const string FILE_NAME = "data.txt";
+	map<string, int> traffic;
+
+	ifstream inputFile(FILE_NAME);
+	if (!inputsFile) {
+		cout << "ERROR: Unable to open the file " << FILE_NAME << ".\n";
+		return 1;
+	}
+
+	string origin;
+	string dest;
+	while (inputFile >> origin) {
+		inputFile >> dest;
+		
+		traffic[origin] = 0;
+		traffic[dest] = 0;
+
+
+	}
+
+
 
 	return 0;
 }
